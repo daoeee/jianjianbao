a6cf391ead8a68f8832a925a23546c8c56a5eda2
Daoeee <spon@spondeMac.local>
2015-05-06 16:04:35 +0800

    新增中文字段
    文件编码改为UTF8

--- models/poster_model.js
+++ models/poster_model.js
@@ -1,11 +1,16 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 var PosterSchema = new Schema({
+    //"海报id" : String
+    //, "标题" : String  
+  
+  
+  
   username: {type: String, unique: false},
   //username: String, 
   subject: String,
-  //"red envelope amount"
-  amount: String,
+  "red envelope amount": String,
+  "红包金额": String,
   timestamp: { type: Date, default: Date.now },
   body: String,
 }, { _id: true });
