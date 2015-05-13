e257410aaa8ec39cd9cf7105c3e1f02e24e576d3
simbacai <simbacai@yahoo.com>
2015-05-13 22:14:36 +0800

    fix action API duplicate issue

--- controllers/action_controller.js
+++ controllers/action_controller.js
@@ -5,15 +5,15 @@ var mongoose = require('mongoose'),
 
 exports.visitPoster = function(req, res) {
 	//For Testing Purpose
-	
+	/*
 	req.body.newAction = {
     "userid" : "55505c4b64b16ccf0e5c6939", 
     "posterid" : "55530fb6cbabc042078b8e96",
     "action" : [{
         "actiontype":"visit"
     }] };
-    req.body.parent = {"parent":"55531cf8fd5ec9cd0b7d1489"};
-    
+    req.body.parent = {"parent":"555351a88631fccf15606bad"};
+    */
   if(req.session.user){
     Action.findOne({_id:req.body.parent.parent})
     .exec(function(err, parentAction) {
@@ -23,21 +23,37 @@ exports.visitPoster = function(req, res) {
           if (!parentAction){
             res.json(400,{errcode:1006});
           } else {
-          		if(parentAction.getChildren({condition:{userid:req.body.newAction.userid}})) {
-          			res.json(403,{errorcode:1000});
-          		} else {
-	          		var newAction = new Action(req.body.newAction);
-	                parentAction.appendChild(newAction,function(err){
-	          		if(err) {
-	            		res.json(400,{errorcode:1006});
-	      			} else {
-	            		res.json(200,newAction);
-	          		}	
-          		});
+          		parentAction.getChildren({condition:{userid:req.body.newAction.userid}}
+          		,function(err,existChildAction){
+          			if (err) {
+          				console.log(err);
+          				res.json(403,{errorcode:1000});
+          			} else {
+          				if(existChildAction.length) {
+          					res.json(403,{errorcode:1000});
+          				} else {
+          					var newAction = new Action(req.body.newAction);
+	               			parentAction.appendChild(newAction,function(err){
+	          				if(err) {
+	            				res.json(400,{errorcode:1006});
+	      					} else {
+	      					parentAction.action.push({"actiontype":"forward"});
+	      					parentAction.save(function(err){
+	      						if(err){
+	      							console.log("enter hereh2");
+	      							res.json(400,{errorcode:1006});
+	      						} else {
+	      							res.json(200,newAction);
+	      						}
+	      					});	
+          					}
+          				});		
+          				}
+          			}
+          		}); 
         	}       		
           }  
-        } 
-      }); 
+        }); 
   }else {
     res.json(403,{errorcode:1000});
   }    
@@ -56,24 +72,35 @@ exports.rootPoster = function(req, res) {
             res.json(400,{errcode:1006});
           } else {
           		if(req.session.user == poster.create_userid){
-          			if(Action.findOne({posterid:poster._id})){
-          				res.json(403,{errorcode:1000});
-          			} else {
-          				var newAction = new Action;
-          				newAction.userid = req.session.user;
-          				newAction.posterid = poster._id;
-          				newAction.save(function(err){
-                  		if(err) {
-                    		res.json(400,{errorcode:1006});
-                  		} else {
-                  			res.json(200,poster);
-                  		}
-                		});
-          			} 
+          			console.log(poster._id);
+          			Action.findOne({posterid:poster._id})
+          			.exec(function(err,existAction){
+          				if(err){
+          					res.json(403,{errorcode:1000});
+          				} else {
+          					if(!existAction) {
+          						var newAction = new Action;
+		          				newAction.userid = req.session.user;
+		          				newAction.posterid = poster._id;
+		          				newAction.save(function(err){
+		                  		if(err) {
+		                    		res.json(400,{errorcode:1006});
+		                  		} else {
+		                  			res.json(200,poster);
+		                  		}
+		                		});
+          					} else {
+          						res.json(403,{errorcode:1000});
+          					}	
+          				}
+          			});
           		} else {
           			res.json(403,{errorcode:1000});
           		}
           	} 
-          }});
-	}    
+        }
+    	});
+	} else {
+        res.json(403,{errorcode:1000});
+    } 
 };  
--- models/action_model.js
+++ models/action_model.js
@@ -6,7 +6,7 @@ var ActionSchema = new Schema({
     "userid" : {type: String, maxlength: 32}, 
     "posterid" : {type: String, maxlength: 32},
     "action" : [{
-        "actiontype":{type:String, enum:"visit,comment,propose,commit".split(",")},
+        "actiontype":{type:String, enum:"visit,forward,comment,propose,commit".split(",")},
         "actionContent":{type:String, maxlength:300},
         "update_timestamp":{type:Date, default:Date.now}
     } ]
